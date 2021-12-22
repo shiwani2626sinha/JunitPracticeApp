8199993d3cb8b77c3192250b68c631b18cc007b8
shiwani2626sinha <shiwani2626@gmail.com>
2021-12-23 01:40:14 +0530

    "class_codes_added" "clinicMain_code_added"

--- src/main/java/patientIntake/ClinicMain.java
+++ src/main/java/patientIntake/ClinicMain.java
@@ -1,4 +1,36 @@
 package patientIntake;
 
+import java.util.Scanner;
+
 public class ClinicMain {
+    private static ClinicCalendar calendar;
+
+    public static void main(String[] args) throws Throwable{
+        calendar = new ClinicCalendar();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Welcome to Patient intake Computer System!\n\n");
+        String lastOption = "";
+        while(!lastOption.equalsIgnoreCase("x")){
+            lastOption = displayMenu(scanner);
+        }
+        System.out.println("\nExiting System...\n");
+    }
+
+    private static String displayMenu(Scanner scanner) throws  Throwable{
+        System.out.println("Please select an option");
+        System.out.println("1. Enter a Patient Appointment");
+        System.out.println("2.View The Appointment List");
+        System.out.println("Enter x for exit");
+        System.out.println("Option: ");
+        String option = scanner.next();
+        switch (option){
+            case "1" : performPatientEntry(scanner);
+                        return option;
+            case "2" : performAllAppointments();
+                        return  option;
+            default: System.out.println("Invalid option");
+                        return option;
+        }
+    }
+
 }
